21a2f713d3601dd858f2d5181cb293ace7563f3f
Guillermo Beltri <gobeltri@gmail.com>
2016-04-11 15:59:03 +0200

    Rails new + Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NlocalAgenda::Application.config.secret_key_base = '3c54162fe01b0f8747b7b2d9be8aafe40d362dd2e20b31b7bc91f7c310f17d8eba7a2dd6c271cb18f03f6e2e5df1167d44de8caaf7ce28415cbfe131817ac2f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NlocalAgenda::Application.config.session_store :cookie_store, key: '_nlocal_agenda_session'
