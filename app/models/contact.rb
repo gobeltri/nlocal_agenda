588d4cea7806f0d7e757a8b8e857f57210b598b0
Guillermo Beltri <gobeltri@gmail.com>
2016-04-12 12:07:33 +0200

    Added required validations to Contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,12 +1,15 @@
-# t.integer  "user_id",       null: false
-# t.string   "first_name",    null: false
-# t.string   "last_name",     null: false
-# t.string   "email",         null: false
-# t.string   "avatar_url"
+# t.integer  "user_id",                         null: false
+# t.string   "first_name",          limit: 255, null: false
+# t.string   "last_name",           limit: 255, null: false
+# t.string   "email",               limit: 255, null: false
 # t.text     "phone_numbers"
-# t.string   "contact_type"
+# t.string   "contact_type",        limit: 255
 # t.datetime "created_at"
 # t.datetime "updated_at"
+# t.string   "avatar_file_name"
+# t.string   "avatar_content_type"
+# t.integer  "avatar_file_size"
+# t.datetime "avatar_updated_at"
 
 class Contact < ActiveRecord::Base
 
@@ -15,4 +18,9 @@ class Contact < ActiveRecord::Base
   serialize :phone_numbers, Array
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
 end
