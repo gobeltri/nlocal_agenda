4076eea7a57fa71da65764496efe5b15eff3b95e
Guillermo Beltri <gobeltri@gmail.com>
2016-04-12 11:10:31 +0200

    Adding phone numbers as an array - maximum 5

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,16 @@
+# t.integer  "user_id",       null: false
+# t.string   "first_name",    null: false
+# t.string   "last_name",     null: false
+# t.string   "email",         null: false
+# t.string   "avatar_url"
+# t.text     "phone_numbers"
+# t.string   "contact_type"
+# t.datetime "created_at"
+# t.datetime "updated_at"
+
 class Contact < ActiveRecord::Base
 
   belongs_to :user
+
+  serialize :phone_numbers, Array
 end
